8b661c4768bdf0bf66b75f7c511380446280e2fb
Yatharth Sharma <yatharth0045@gmail.com>
2020-10-25 22:09:39 +0530

    Update program to print name

--- file1.java
+++ file1.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
-        System.out.println("hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello "+sc.next());
     }
 }
